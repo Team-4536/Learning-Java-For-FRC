02bf521f4e6ed6d02a6b298f9cd21986eb997844
team4536 <team4536@10.11.33.66>
2015-11-19 19:10:05 -0600

    I(Olivia) added limit switch functionality.

--- Learning_Java_For_FRC/src/org/usfirst/frc/team4536/robot/subsystems/Elevator.java
+++ Learning_Java_For_FRC/src/org/usfirst/frc/team4536/robot/subsystems/Elevator.java
@@ -1,17 +1,25 @@
 package org.usfirst.frc.team4536.robot.subsystems;
-
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  *
  */
 public class Elevator extends Subsystem {
 	Jaguar jmotor;
+	DigitalInput limitSwitch;
     public Elevator(int jaguarChannel){
      jmotor = new Jaguar(jaguarChannel);
+     limitSwitch = new DigitalInput(3);
     }
+     //TODO  Make public boolean getLimitSwitchValueS
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    public boolean sensor(){
+    	if(limitSwitch.get()){
+    		return true;
+    	}return false;
+    }
     public void drive(double speed){
     	jmotor.set(speed);
     }
@@ -21,4 +29,6 @@ public class Elevator extends Subsystem {
         //setDefaultCommand(new MySpecialCommand());
     }
 }
+
+
 
